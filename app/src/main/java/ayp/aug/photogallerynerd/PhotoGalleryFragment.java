f1d58a826d1b5b6fa3e9d63de435525ce49add05
Tanaphon <tanaphon.a@augmentis.biz>
2016-09-16 22:07:31 +0700

    Poll service notification

--- app/src/main/java/ayp/aug/photogallerynerd/PhotoGalleryFragment.java
+++ app/src/main/java/ayp/aug/photogallerynerd/PhotoGalleryFragment.java
@@ -56,9 +56,6 @@ public class PhotoGalleryFragment extends Fragment {
         setHasOptionsMenu(true);
         updateItems();
 
-        Intent i = PollService.newIntent(getActivity());
-        getActivity().startService(i);
-
         Handler responseHandler = new Handler();
 
         mThumbnailDownloader = new ThumbnailDownloader<>(responseHandler);
@@ -110,6 +107,12 @@ public class PhotoGalleryFragment extends Fragment {
                 searchView.setQuery(query, false);
             }
         });
+
+        MenuItem toggleItem = menu.findItem(R.id.menu_item_toggle_polling);
+        if (PollService.isServiceAlarmOn(getActivity()))
+            toggleItem.setTitle(R.string.stop_polling);
+        else
+            toggleItem.setTitle(R.string.start_polling);
     }
 
     @Override
@@ -118,6 +121,11 @@ public class PhotoGalleryFragment extends Fragment {
             case R.id.menu_item_clear:
                 QueryPreferences.setStoredQuery(getActivity(), null);
                 updateItems();
+                return true;
+            case R.id.menu_item_toggle_polling:
+                boolean shouldStartAlarm = !PollService.isServiceAlarmOn(getActivity());
+                PollService.setServiceAlarm(getActivity(), shouldStartAlarm);
+                getActivity().invalidateOptionsMenu();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
--- app/src/main/java/ayp/aug/photogallerynerd/PollService.java
+++ app/src/main/java/ayp/aug/photogallerynerd/PollService.java
@@ -1,17 +1,29 @@
 package ayp.aug.photogallerynerd;
 
+import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.os.SystemClock;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
+
+import java.util.List;
 
 /**
  * Created by Tanaphon on 9/16/2016.
  */
 public class PollService extends IntentService {
     private static final String TAG = "PollService";
+    private static final long POLL_INTERVAL = 1000 * 60;// 60secs
+//    private static final long POLL_INTERVAL = AlarmManager.INTERVAL_FIFTEEN_MINUTES;
 
-    public static Intent newIntent(Context context){
+    public static Intent newIntent(Context context) {
         return new Intent(context, PollService.class);
     }
 
@@ -19,8 +31,80 @@ public class PollService extends IntentService {
         super(TAG);
     }
 
+    public static void setServiceAlarm(Context context, boolean isOn) {
+        Intent i = PollService.newIntent(context);
+        PendingIntent pi = PendingIntent.getService(context, 0, i, 0);
+        AlarmManager alarmManager =
+                (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        if (isOn)
+            alarmManager.setInexactRepeating(
+                    AlarmManager.ELAPSED_REALTIME,
+                    SystemClock.elapsedRealtime(),
+                    POLL_INTERVAL,
+                    pi
+            );
+        else
+            alarmManager.cancel(pi);
+    }
+
+    public static boolean isServiceAlarmOn(Context context) {
+        Intent i = PollService.newIntent(context);
+        PendingIntent pi = PendingIntent
+                .getService(context, 0, i, PendingIntent.FLAG_NO_CREATE);
+        return pi != null;
+    }
+
     @Override
     protected void onHandleIntent(Intent intent) {
-        Log.i(TAG, "Receive an intent: " + intent);
+        if (!isNetworkAvailableAndConnected()) {
+            return;
+        }
+        String query = QueryPreferences.getStoredQuery(this);
+        String lastResultId = QueryPreferences.getLastResultId(this);
+
+        List<GalleryItem> items;
+
+        if (query == null)
+            items = new FlickrFetchr().fetchRecentPhotos();
+        else
+            items = new FlickrFetchr().searchPhotos(query);
+
+        if (items.size() == 0)
+            return;
+
+        String resultId = items.get(0).getId();
+        if (resultId.equals(lastResultId))
+            Log.i(TAG, "Got old result id " + resultId);
+        else {
+            Log.i(TAG, "Got new result id " + resultId);
+
+            Resources resources = getResources();
+            Intent i = PhotoGalleryActivity.newIntent(this);
+            PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+
+            Notification notification = new NotificationCompat.Builder(this)
+                    .setTicker(resources.getString(R.string.new_pictures_title))
+                    .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                    .setContentTitle(resources.getString(R.string.new_pictures_title))
+                    .setContentText(resources.getString(R.string.new_pictures_text))
+                    .setContentIntent(pi)
+                    .setAutoCancel(true)
+                    .build();
+
+            NotificationManagerCompat notificationManagerCompat =
+                    NotificationManagerCompat.from(this);
+            notificationManagerCompat.notify(0, notification);
+        }
+        QueryPreferences.setLastResultId(this, resultId);
+
+    }
+
+    private boolean isNetworkAvailableAndConnected() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        boolean isNetworkAvailable = cm.getActiveNetworkInfo() != null;
+        boolean isNetworkConnected = isNetworkAvailable && cm.getActiveNetworkInfo().isConnected();
+        return isNetworkConnected;
     }
 }
